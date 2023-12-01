2fd87a05e239e050fb8e2283f9df28722ff9c8c1
laurentfrederick <laurent.frederick@icloud.com>
2023-12-01 14:53:01 +0100

    Day01: part2 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,22 +1,30 @@
+import java.lang.StringBuilder
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.sumOf(String::getCalibrationValue)
+        return input
+            .sumOf(String::getCalibrationValueFromFirstAndLastDigitChar)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .map(String::transformSpelledDigitWithDigitChar)
+            .sumOf(String::getCalibrationValueFromFirstAndLastDigitChar)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+    val testInputPart1 = readInput("Day01_part1_test")
+    check(part1(testInputPart1) == 142)
+    val testInputPart2 = readInput("Day01_part2_test")
+    check(part2(testInputPart2) == 281)
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val inputParty1 = readInput("Day01_part1")
+    part1(inputParty1).println()
+    val inputParty2 = readInput("Day01_part2")
+    part2(inputParty2).println()
 }
 
-fun String.getCalibrationValue(): Int {
+fun String.getCalibrationValueFromFirstAndLastDigitChar(): Int {
     var first: Char? = null
     var last: Char? = null
     for (i in this.indices) {
@@ -25,4 +33,26 @@ fun String.getCalibrationValue(): Int {
         if (first != null && last != null) return "$first$last".toInt()
     }
     return 0
+}
+
+fun String.transformSpelledDigitWithDigitChar(): String {
+    val spelledToCharDigit = mapOf(
+        "zero" to '0',
+        "one" to '1',
+        "two" to '2',
+        "three" to '3',
+        "four" to '4',
+        "five" to '5',
+        "six" to '6',
+        "seven" to '7',
+        "eight" to '8',
+        "nine" to '9',
+    )
+    val transform = StringBuilder()
+
+    for (i in indices) {
+        val match = spelledToCharDigit.entries.find { this.startsWith(it.key, i) }
+        transform.append(match?.value ?: this[i])
+    }
+    return transform.toString()
 }
