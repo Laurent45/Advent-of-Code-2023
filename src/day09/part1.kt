8f8a18bbbd7a47886f461a6e311a3350e4d9e231
laurentfrederick <laurent.frederick@icloud.com>
2023-12-09 08:45:35 +0100

    day09: part2 done

--- src/day09/part1.kt
+++ src/day09/part1.kt
@@ -8,13 +8,20 @@ fun main() {
         val values = history.split(" ").map { it.toInt() }
         findPredictionValue(values)
     }.also(::println)
+
+    readInput("Day09_part2").sumOf { history ->
+        val values = history.split(" ").map { it.toInt() }
+        findPredictionValue(values, true)
+    }.also(::println)
 }
 
-fun findPredictionValue(values: List<Int>): Int {
+fun findPredictionValue(values: List<Int>, backwards: Boolean = false): Int {
     if (values.all { it == 0 }) return 0
 
-    // [1, 2, 3, 4, 5] => [ [1, 2], [2, 3], [3, 4], [4, 5]]
+    // [1, 2, 3, 4, 5] => [ [1, 2], [2, 3], [3, 4], [4, 5] ] => [1, 1, 1, 1]
     val steps = values.windowed(2, 1).map { it[1] - it[0] }
-    val prediction = findPredictionValue(steps)
-    return values.last() + prediction
+
+    val prediction = findPredictionValue(steps, backwards)
+
+    return if (backwards) values.first() - prediction else values.last() + prediction
 }
