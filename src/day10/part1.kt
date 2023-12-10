9a92c2351004b1bffef2b43e159441a44de5a7b9
laurentfrederick <laurent.frederick@icloud.com>
2023-12-10 17:14:46 +0100

    day10: part1 refacto

--- src/day10/part1.kt
+++ src/day10/part1.kt
@@ -1,84 +1,85 @@
 package day10
 
+import println
 import readInput
 
+
 fun main() {
+
+    // Symbol of pipe map to in/out direction
+    val pipes = mapOf(
+        '|' to Pair(Direction.NORTH, Direction.SOUTH),
+        '-' to Pair(Direction.EAST, Direction.WEST),
+        'J' to Pair(Direction.NORTH, Direction.WEST),
+        'L' to Pair(Direction.NORTH, Direction.EAST),
+        'F' to Pair(Direction.SOUTH, Direction.EAST),
+        '7' to Pair(Direction.SOUTH, Direction.WEST)
+    )
+
     var startedPosition: Position? = null
 
-    val diagram: List<List<Pipe?>> = readInput("Day10_part1").mapIndexed { idx, line ->
+    val diagram: List<List<Char>> = readInput("Day10_part1").mapIndexed { idx, line ->
 
         val column = line.indexOf("S")
         if (column != -1) startedPosition = Position(idx, column)
 
-        line.map { it.toPipe() }
+        line.toList()
     }
 
     val maxRows = diagram.size - 1
     val maxColumns = diagram[0].size - 1
 
-    val loops: MutableList<MutableList<Position>> = mutableListOf()
+    Direction.entries.map { startedDirection ->
 
-    for (direction in Direction.entries) {
-
-        var currentDirection = direction
         var currentPosition = startedPosition!!
+        var currentDirection = startedDirection
+        val crossPositions = mutableListOf<Position>()
 
-        val loop = mutableListOf<Position>()
         loop_tiles@ while (true) {
             val nextPosition = currentPosition.nextPosition(currentDirection)
-            loop.add(nextPosition)
 
             if (!(nextPosition.row in 0..maxRows && nextPosition.column in 0..maxColumns)) break@loop_tiles
 
-            val nextPipe = diagram[nextPosition.row][nextPosition.column] ?: break@loop_tiles
+            val nextPipe = diagram[nextPosition.row][nextPosition.column]
 
-            if (currentDirection canBeConnected nextPipe.symbol) {
-                currentPosition = nextPosition
-
-                val directionIn = when(currentDirection) {
-                    Direction.NORTH -> Direction.SOUTH
-                    Direction.SOUTH -> Direction.NORTH
-                    Direction.WEST -> Direction.EAST
-                    Direction.EAST -> Direction.WEST
-                }
-
-                currentDirection = if (directionIn == nextPipe.connectedBy.first) nextPipe.connectedBy.second else nextPipe.connectedBy.first
-            } else {
+            if (nextPipe == '.') break@loop_tiles
+            if (nextPipe == 'S') {
+                crossPositions.add(nextPosition)
                 break@loop_tiles
             }
-        }
-        loops += loop
-    }
 
-    loops.filter { it.last() == startedPosition }.let {
-        it[0].zip(it[1]).indexOfFirst { zip -> zip.first == zip.second }.also(::println)
+            val enterBy = currentDirection.reverse()
+            val connectedBy = pipes[nextPipe] ?: error("Shouldn't possible")
+
+            currentDirection = if (connectedBy.first == enterBy) connectedBy.second
+            else if (connectedBy.second == enterBy) connectedBy.first
+            else break@loop_tiles
+
+            crossPositions.add(nextPosition)
+            currentPosition = nextPosition
+        }
+
+        crossPositions
     }
+        .filter { it.isNotEmpty() }
+        .first { it.last() == startedPosition }
+        .let { it.size / 2 }
+        .println()
 }
 
-fun Position.nextPosition(to: Direction) = when(to) {
+fun Position.nextPosition(to: Direction) = when (to) {
     Direction.NORTH -> Position(row - 1, column)
     Direction.SOUTH -> Position(row + 1, column)
     Direction.EAST -> Position(row, column + 1)
     Direction.WEST -> Position(row, column - 1)
 }
 
-infix fun Direction.canBeConnected(symbol: Char) = when(this) {
-    Direction.SOUTH -> "|LJ".any { it == symbol }
-    Direction.NORTH -> "|F7".any { it == symbol }
-    Direction.EAST -> "-J7".any { it == symbol }
-    Direction.WEST -> "-FL".any { it == symbol }
-}
-
-fun Char.toPipe() = when {
-    this == '|' -> Pipe(this, Direction.NORTH to Direction.SOUTH)
-    this == '-' -> Pipe(this, Direction.WEST to Direction.EAST)
-    this == 'L' -> Pipe(this, Direction.EAST to Direction.NORTH)
-    this == 'J' -> Pipe(this, Direction.WEST to Direction.NORTH)
-    this == '7' -> Pipe(this, Direction.WEST to Direction.SOUTH)
-    this == 'F' -> Pipe(this, Direction.EAST to Direction.SOUTH)
-    else -> null
+fun Direction.reverse() = when (this) {
+    Direction.SOUTH -> Direction.NORTH
+    Direction.NORTH -> Direction.SOUTH
+    Direction.EAST -> Direction.WEST
+    Direction.WEST -> Direction.EAST
 }
 
 enum class Direction { NORTH, SOUTH, EAST, WEST }
 data class Position(val row: Int, val column: Int)
-data class Pipe(val symbol: Char, val connectedBy: Pair<Direction, Direction>)
